35bf8ea9a14e8436180e77a57f586c5388753854
Hooooooon <shonn2323@gmail.com>
2021-09-24 23:19:38 +0900

    spring project 초기

---
+++ src/main/java/com/example/hoon_shop/HoonShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoon_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoonShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoonShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hoon_shop/HoonShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoon_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoonShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
