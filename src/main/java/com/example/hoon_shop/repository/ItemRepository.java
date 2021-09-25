ee15a1ac26388d593ed3c546f685672312c1d4da
Hooooooon <shonn2323@gmail.com>
2021-09-25 23:11:34 +0900

    item Repository JPA 메소드 추가 및 테스트 코드 추가

--- src/main/java/com/example/hoon_shop/repository/ItemRepository.java
+++ src/main/java/com/example/hoon_shop/repository/ItemRepository.java
@@ -2,10 +2,27 @@ package com.example.hoon_shop.repository;
 
 import com.example.hoon_shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByItemName(String itemName);
+
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetatil);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    // Native 쿼리문 사용
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc",
+            nativeQuery = true)
+    List<Item> findByItemDetailNative(@Param("itemDetail") String itemDetail);
 }
+
--- src/test/java/com/example/hoon_shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/hoon_shop/repository/ItemRepositoryTest.java
@@ -2,6 +2,8 @@ package com.example.hoon_shop.repository;
 
 import com.example.hoon_shop.constant.ItemSellStatus;
 import com.example.hoon_shop.entity.Item;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +21,11 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @AfterEach
+    public void cleanup() {
+        itemRepository.deleteAll();
+    }
 
     @Test
     public void 상품_저장_테스트() {
@@ -56,5 +63,50 @@ class ItemRepositoryTest {
         List<Item> items = itemRepository.findByItemName("테스트 상품0");
 
         assertThat(items.get(0).getItemName()).isEqualTo("테스트 상품0");
+    }
+
+    @Test
+    public void 상품명_또는_상품상세설명_조회_테스트() {
+        this.createItemList();
+        List<Item> items = itemRepository.findByItemNameOrItemDetail("테스트 상품0", "테스트 상품 상세 설명9");
+
+        assertThat(items.get(0).getItemName()).isEqualTo("테스트 상품0");
+        assertThat(items.get(1).getItemName()).isEqualTo("테스트 상품9");
+    }
+
+    @Test
+    public void 상품가격_LessThan_테스트() {
+        this.createItemList();
+        List<Item> items = itemRepository.findByPriceLessThan(10004);
+        for (Item item : items) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    public void 상품가격_LessThan_내림차순_테스트() {
+        this.createItemList();
+        List<Item> items = itemRepository.findByPriceLessThanOrderByPriceDesc(10004);
+        for (Item item : items) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    public void Query를_이용한_상품조회_테스트() {
+        this.createItemList();
+        List<Item> items = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for (Item item : items) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    public void NativeQuery를_이용한_상품조회() {
+        this.createItemList();
+        List<Item> items = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for (Item item : items) {
+            System.out.println(item.toString());
+        }
     }
 }
