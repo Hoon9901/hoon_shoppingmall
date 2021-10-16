c1b97ede1b75fc30401c5f949cdb74148413cd22
Hooooooon <shonn2323@gmail.com>
2021-10-16 23:59:30 +0900

    주문 엔티티 설계

---
+++ src/main/java/com/example/hoon_shop/entity/Order.java
@@ -0,0 +1,32 @@
+package com.example.hoon_shop.entity;
+
+import com.example.hoon_shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
