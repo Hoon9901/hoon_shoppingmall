02fd9c2b927f5b03620c553d23658ff7ef795939
Hoon9901 <shonn.dev@gmail.com>
2021-12-28 22:42:28 +0900

    Fix: 중복 매핑 오류 해결

--- src/main/java/com/example/hoon_shop/controller/MainController.java
+++ src/main/java/com/example/hoon_shop/controller/MainController.java
@@ -8,8 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/")
-    public String main(){
-        return "Hello World";
-    }
 }
